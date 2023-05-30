0920cbac77b08ddbf792685deb887372e7bdd051
ichwanmrz <ichwan.mrz@gmail.com>
2023-05-30 16:32:07 +0700

    latiham

---
+++ Documents/MDB1/Arsip FE/Praktek FE/belajar-js/home.js
@@ -0,0 +1,119 @@
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+// let result;
+
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+// let mahasiswa = {
+//     name : "Ichwan",
+//     age : 30,
+//     hobbies : ["coding", "teaching", "traveling"],
+//     isMarried : true
+//     }
+//     document.write(mahasiswa.nama + '&nbsp' + 'hobinya' + '&nbsp'+  mahasiswa.hobbies[1])
+    
+
+// for (let i = 0; i < 100; i++) {
+//     document.write("Hello World!<br>");
+//   }
+
+
+// console.log("Loop start");
+// let a = 0;
+// do {
+//     document.write(a+ '<br>');
+//     a++;
+// } 
+// while (a < 10 )
+   
+//   console.log(i);
+
+
+
+// let result;
+
+
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//          result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         document.getElementById('demo').innerHTML = 'hasilnya adalah' + ' ' + result;
+//         break;
+
+//     case '-':
+//          result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         document.getElementById('demo').innerHTML = 'hasilnya adalah' + ' ' + result;
+//         break;
+
+//     case '*':
+//          result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         document.getElementById('demo').innerHTML = 'hasilnya adalah' + ' ' + result;
+//         break;
+
+//     case '/':
+//          result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         document.getElementById('demo').innerHTML = 'hasilnya adalah' + ' ' + result;
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+
+
+//     cek[i]
+// }
+
+
+let i = document.getElementById('tes')
+let a = document.getElementsByClassName('label')
+let b = document.getElementsByName('judul')
+let c = document.getElementsByTagName('div')
+let d = document.querySelector('h1')
+let e = document.querySelectorAll('div')
+
+
+console.log(i);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// let i = 0;
+// let x = 'ichwan';
+<h1>welcome</h1>
+// const date = new Date();
+// const n = date.toDateString();
+// const time = date.toLocaleTimeString();
+// document.write('Hari ini adalah hari : '+ '<br>' + n + '<br> '+ time + '<br><br>')
+
+// console.log('Date: ' + n);
+// console.log('Time: ' + time + '<br>');
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubled = numbers.map((number) => number * 5);
+// document.write(doubled)
+// console.log(doubled);
